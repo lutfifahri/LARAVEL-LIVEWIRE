d74084b30ad87f6ef8cc136731db0cc8cb0023d0
Lutfi Fahri <lfahri959@gmail.com>
2023-11-15 23:17:44 +0700

    first commit

---
+++ app/Livewire/Employee.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Employee as ModelsEmployee;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class Employee extends Component
+{
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
+    public $nama;
+    public $email;
+    public $alamat;
+    public $updateData = false;
+    public $employee_id;
+    public $katakunci;
+    public $employe_selected_id = [];
+
+    public function store()
+    {
+        $rules = [
+            'nama'      => 'required',
+            'email'     => 'required|email',
+            'alamat'    => 'required',
+        ];
+        // $validated = $this->validate($rules);
+        $pesan = [
+            'nama.required' => 'Nama wajib diisi',
+            'email.required' => 'Email wajib diisi',
+            'email.email' => 'Format email tidak sesuai',
+            'alamat.required' => 'Alamat wajib diisi',
+        ];
+        $validated = $this->validate($rules, $pesan);
+        ModelsEmployee::create($validated);
+        session()->flash('message', 'Data berhasil di simpan');
+
+        $this->clear();
+    }
+
+    public function edit($id){
+        $data = ModelsEmployee::find($id);
+        $this->nama = $data->nama;
+        $this->email = $data->email;
+        $this->alamat = $data->alamat;
+
+        $this->updateData = true;
+        $this->employee_id = $id;
+    }
+
+    public function update()
+    {
+        $rules = [
+            'nama'      => 'required',
+            'email'     => 'required|email',
+            'alamat'    => 'required',
+        ];
+        // $validated = $this->validate($rules);
+        $pesan = [
+            'nama.required' => 'Nama wajib diisi',
+            'email.required' => 'Email wajib diisi',
+            'email.email' => 'Format email tidak sesuai',
+            'alamat.required' => 'Alamat wajib diisi',
+        ];
+        $validated = $this->validate($rules, $pesan);
+        $data = ModelsEmployee::find($this->employee_id);
+        $data->update($validated);
+        session()->flash('message', 'Data berhasil di Update');
+
+        $this->clear();
+    }
+
+    public function clear() 
+    {
+        $this->nama = '';
+        $this->email = '';
+        $this->alamat = '';
+
+        $this->updateData = false;
+        $this->employee_id = '';
+        $this->employe_selected_id = [];
+    }
+
+    public function delete()
+    {
+        if ($this->employee_id !='') {
+            $id = $this->employee_id;
+            ModelsEmployee::find($id)->delete();
+        }
+
+        if (count($this->employe_selected_id)) {
+            for ($x=0; $x<count($this->employe_selected_id) ; $x++) { 
+                ModelsEmployee::find($this->employe_selected_id[$x])->delete();
+            }
+        }
+        session()->flash('message', 'Data berhasil di Delete');
+        $this->clear();
+        
+    }
+
+    public function delete_confirmation($id)
+    {
+        if ($id !="") {
+            $this->employee_id = $id;
+        }
+    }
+
+    public function render()
+    {
+        if ($this->katakunci != null) {
+            $data =  ModelsEmployee::where('nama','like','%' . $this->katakunci . '%')
+            ->orWhere('email','like','%' . $this->katakunci . '%')
+            ->orWhere('alamat','like','%' . $this->katakunci . '%')
+            ->orderBy('nama','asc')->paginate(2);
+        } else {
+            $data =  ModelsEmployee::orderBy('nama','asc')->paginate(2);
+        }
+        return view('livewire.employee', ['dataEmployees' => $data]);
+    }
+}
